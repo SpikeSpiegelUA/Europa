6f05ac29df7eba19ec34ba50379822e44f0bd5c6
Ivan <ivansok110503@gmail.com>
2024-12-02 22:22:29 +0200

    Project creation work
    -Created a base class ViewModelBase, that contains an event for children property changes;
    -Set default values for textboxes in NewProjectView;
    -Started a work on the DataContractSerializer.

---
+++ EuropaEditor/GameProject/Backend/NewProject.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuropaEditor.GameProject.Backend
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> ProjectFolders { get; set; }
+    }
+    internal class NewProject : ViewModelBase
+    {
+        //TODO: get the path from the installation location.
+        private readonly string _templatePath = @"..\..\EuropaEditor\ProjectTemplates\";
+        private string _name = "New project";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (!_name.Equals(value))
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\EuropaProject\";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (!_path.Equals(value))
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+        public NewProject()
+        {
+            try
+            {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Any());
+                foreach(var file in templatesFiles)
+                {
+                    var template = new ProjectTemplate()
+                    {
+                        ProjectType = "Empty Project",
+                        ProjectFile = "project.europa",
+                        ProjectFolders = new List<string> { ".Europa", "Content", "GameCode" }
+                    }; 
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: log error.
+            }
+        }
+    }
+}
---
+++ EuropaEditor/Utilities/Serializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuropaEditor.Utilities
+{
+   public static class Serializer
+    {
+        public static void ToFile<T>(T instance, string path)
+        {
+            try
+            {
+                var fileStream = new FileStream(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(fileStream, instance);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: log error.
+            }
+        }
+    }
+}
