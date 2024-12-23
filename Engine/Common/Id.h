7555330bf1534a5a38ca11fc29512ed281794779
Ivan <ivansok110503@gmail.com>
2024-12-23 10:12:51 +0200

    Started C++ part.
    -Finally started working on the C++ part of the engine;
    -Added header file for engine's own primitive types;
    -Started working on the entity ID system;
    -Added bitmasks for generation and index parts of the ID.

---
+++ Engine/Common/Id.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "CommonHeaders.h"
+
+namespace europa::id {
+	using id_type = uint32;
+
+	constexpr uint32 generation_bits{ 8 };
+	constexpr uint32 index_bits{ sizeof(id_type) * 8 - generation_bits };
+
+	constexpr id_type index_mask{ (id_type{1} << index_bits) - 1};
+	constexpr id_type generation_mask{ (id_type{1} << generation_bits) - 1 };
+	constexpr id_type id_mask{ id_type{-1} };
+
+	using generation_type = std::conditional_t<generation_bits <= 16, std::conditional_t<generation_bits <= 8, uint8, uint16>, uint32>;
+}
---
+++ Engine/Common/PrimitiveTypes.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "stdint.h"
+
+//Unsigned integers.
+
+using uint64 = uint64_t;
+using uint32 = uint32_t;
+using uint16 = uint16_t;
+using uint8 = uint8_t;
+
+//Signed integers.
+
+using int64 = int64_t;
+using int32 = int32_t;
+using int16 = int16_t;
+using int8 = int8_t;
+
+constexpr uint64 uint64_invalid_id{ 0xffff'ffff'ffff'ffffui64 };
+constexpr uint32 uint32_invalid_id{ 0xffff'ffffui32 };
+constexpr uint16 uint16_invalid_id{ 0xffffui16 };
+constexpr uint8 uint8_invalid_id{ 0xffui8 };
