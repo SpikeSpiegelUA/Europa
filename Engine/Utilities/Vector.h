98319939f822f70101ccefedade675ea592907b1
Ivan Sokalskyi <ivansok110503@gmail.com>
2026-04-23 23:51:03 +0300

    Free-list container
    -Wrote overwrites, push back, emplace and resize functions for the Free List container.

--- Engine/Utilities/Vector.h
+++ Engine/Utilities/Vector.h
@@ -39,10 +39,154 @@ namespace Europa::Utilities {
 			vector.Reset();
 		}
 
+		//Copy-assignment operator. Clears this vector and copies items
+		//from another vector. The items must be copyable.
+		constexpr Vector& operator=(const Vector& vector) {
+			assert(this != std::addressof(vector));
+			if (this != std::addressof(vector)) {
+				Clear();
+				Reserve(vector.size);
+				for (auto& item : vector
+					EmplaceBack(item));
+				assert(size == vector.size);
+			}
+
+			return *this;
+		}
+
+		//Move-assignment operator. Frees all resources in this vector and
+		//moves the other vector into this one.
+		constexpr Vector& operator=(Vector&& vector) {
+			assert(this != std::addressof(vector));
+			if (this != std::addressof(vector)) {
+				Destroy();
+				Move(vector);
+			}
+
+			return *this;
+		}
+
+		//Destructs the vector and its items as specified in template argument
+		~Vector() {
+			Destroy();
+		}
+
+		//Inserts an item at the end of the vector by copying 'value'.
+		constexpr void PushBack(const T& value) {
+			EmplaceBack(value);
+		}
+
+		//Inserts an item at the end of the vector by moving 'value'.
+		constexpr void PushBack(const T&& value) {
+			EmplaceBack(std::move(value));
+		}
+
+
+		//Copy- or move-constructs an item at the end of the vector.
+		template<typename... params>
+		constexpr decltype(auto) EmplaceBack(params&&... p) {
+			if (size == capacity) {
+				Reserve(((capacity + 1) * 3) >> 1); // reserve 50% more.
+			}
+			assert(size < capacity);
+
+			new(std::addressof(data[size])) T(std::forward<params>(p)...);
+			++size;
+			return data[size - 1];
+		}
+
+		//Resize the vector and initializes new items with their default value.
+		constexpr void Resize(uint64 newSize) {
+			static_assert(std::is_default_constructible_v<T>, "Type must be default-constructible.");
+
+			if (newSize > size) {
+				Reserve(newSize);
+				while (size < newSize) {
+					EmplaceBack();
+				}
+			}
+			else if (newSize < size) {
+				if constexpr (destruct) {
+					DestructRange(newSize, size);
+				}
+			}
+
+			//Do nothing if newSize == size.
+			assert(newSize == size);
+		}
+
+		//Resize the vector and initializes new items by copying 'value'.
+		constexpr void Resize(uint64 newSize, const T& value) {
+			static_assert(std::is_copy_constructible_v<T>, "Type must be copy-constructible.");
+
+			if (newSize > size) {
+				Reserve(newSize);
+				while (size < newSize) {
+					EmplaceBack(value);
+				}
+			}
+			else if (newSize < size) {
+				if constexpr (destruct) {
+					DestructRange(newSize, size);
+				}
+			}
+
+			//Do nothing if newSize == size.
+			assert(newSize == size);
+		}
+
+		//Allocates memory to containt the specified number of items.
+		constexpr void Reserve(uint64 newCapacity) {
+			if (newCapacity > capacity) {
+				//NOTE: realloc() will automatically copy the data in the buffer
+				//if a new region of memory is allocated.
+				void* newBuffer{ realloc(data, newCapacity * sizeof(T)) };
+				assert(newBuffer);
+				if (newBuffer) {
+					data = static_cast<T*>(newBuffer);
+					capacity = newCapacity;
+				}
+			}
+		}
+
+		//Clears the vector and destructs items as specified in template argument.
+		constexpr void Clear() {
+			if constexpr (destruct) {
+				DestructRange(0, size);
+			}
+			size = 0;
+		}
+
 	private:
 		constexpr void Reset() {
 			capacity = 0;
 			size = 0;
+			data = nullptr;
+		}
+
+		constexpr void Move(Vector& vector) {
+			capacity = vector.capacity;
+			size = vector.size;
+			data = vector.data;
+			vector.Reset();
+		
+		constexpr void DestructRange(uint64 first, uint64 last) {
+			assert(destruct);
+			assert(first <= size && last <= size && first <= last);
+			if (data) {
+				while (first != last) {
+					data[first].~T();
+					first++;
+				}
+			}
+		}
+
+		constexpr void Destroy() {
+			assert([&] {return capacity ? data != nullptr : data == nullptr; }());
+			Clear();
+			capacity = 0;
+			if (data)
+				Free(data);
 			data = nullptr;
 		}
 
