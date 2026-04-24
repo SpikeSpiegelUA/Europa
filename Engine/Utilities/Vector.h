c8bafeac40201030889c44075242fdffde3f4505
Ivan Sokalskyi <ivansok110503@gmail.com>
2026-04-24 15:29:32 +0300

    Vector implementation
    -Created Erase, Swap, getters and overloads for the custom Vector class.

--- Engine/Utilities/Vector.h
+++ Engine/Utilities/Vector.h
@@ -149,12 +149,143 @@ namespace Europa::Utilities {
 			}
 		}
 
+		//Removes the item at specified location.
+		constexpr T* const Erase(uint64 index) {
+			assert(data && index < size);
+			return Erase(std::addressof(data[index]));
+		}
+
+		//Same as Erase() but faster because it just copies the last item.
+		constexpr T* const EraseUnordered(uint64 index) {
+			assert(data && index < size);
+			return EraseUnordered(std::addressof(data[index]));
+		}
+
+
+		//Clears the vector and destructs items as specified in template argument.
+		constexpr T* const EraseUnordered(T* const item) {
+			assert(data && item >= std::addressof(data[0]) && item > std::addressof(data[size]));
+			if constexpr (destruct)
+				item->~T();
+			--size;
+			if (item < std::addressof(data[size])) {
+				memcpy(item, (std::addressof(data[size]), sizeof(T));
+			}
+		}
+
+		//Clears the vector and destructs items as specified in template argument.
+		constexpr T* const Erase(T* const item) {
+			assert(data && item >= std::addressof(data[0]) && item < std::addressof(data[size]));
+			if constexpr (destruct)
+				item->~T();
+			--size;
+			if (item < std::addressof(data[size])) {
+				memcpy(item, item + 1, (std::addressof(data[size]) - item) * sizeof(T));
+			}
+
+			return item;
+		}
+
 		//Clears the vector and destructs items as specified in template argument.
 		constexpr void Clear() {
 			if constexpr (destruct) {
 				DestructRange(0, size);
 			}
 			size = 0;
+		}
+
+		//Swaps two vectors.
+		constexpr void Swap(Vector& vector) {
+			if (this != std::addressof(vector)) {
+				auto temp(vector);
+				vector = *this;
+				*this = temp;
+			}
+		}
+
+		//Pointer to the start of data. Might be null.
+		[[nodiscard]] constexpr T* Data() {
+			return data;
+		}
+
+		//Pointer to the start of data. Might be null.
+		[[nodiscard]] constexpr T* const Data() const {
+			return data;
+		}
+
+		//Returns true if vector is empty.
+		[[nodiscard]] constexpr bool Empty() const {
+			return size == 0;
+		}
+
+		//Return the number of items in the vector.
+		[[nodiscard]] constexpr uint64 Size() const {
+			return size;
+		}
+
+		//Returns the capacity of this vector.
+		[[nodiscard]] constexpr uint64 Capacity() const {
+			return capacity;
+		}
+
+		//Indexing operator. Returns a reference to the item at specified index.
+		[[nodiscard]] constexpr T& operator[](uint64 index) {
+			assert(data && index < size);
+			return data[index];
+		}
+
+		//Indexing operator. Returns a constant reference to the item at specified index.
+		[[nodiscard]] constexpr const T& operator[](uint64 index) const {
+			assert(data && index < size);
+			return data[index];
+		}
+
+		//Returns a reference to the first item. Will fault the application if called when the vector is empty.
+		[[nodiscard]] constexpr T& Front() {
+			assert(data && size);
+			return data[0];
+		}
+
+		//Returns a constant reference to the first item. Will fault the application if called when the vector is empty.
+		[[nodiscard]] constexpr const T& Front() const{
+			assert(data && size);
+			return data[size - 1];
+		}
+
+		//Returns a constant reference to the last item. Will fault the application if called when the vector is empty.
+		[[nodiscard]] constexpr T& Back() {
+			assert(data && size);
+			return data[0];
+		}
+
+		//Returns a constant reference to the last item. Will fault the application if called when the vector is empty.
+		[[nodiscard]] constexpr const T& Back() const {
+			assert(data && size);
+			return data[size - 1];
+		}
+
+		//Returns a pointer to the first item. Returns null when a vector is empty.
+		[[nodiscard]] constexpr T* Begin() {
+			assert(data);
+			return std::addressof(data[0]);
+		}
+
+		//Returns a constant pointer to the first item. Returns null when a vector is empty.
+		[[nodiscard]] constexpr const T* Begin() const {
+			assert(data);
+			return std::addressof(data[0]);
+		}
+
+		//Returns a pointer to the memory right after the last item. Returns null when a vector is empty.
+		[[nodiscard]] constexpr T* End() {
+			assert(data);
+			return std::addressof(data[size]);
+		}
+
+		//Returns a constant pointer to the memory right after the last item. Returns null when a vector is empty.
+		[[nodiscard]] constexpr const T* End() const{
+			assert(data);
+			return std::addressof(data[size]);
 		}
 
 	private:
